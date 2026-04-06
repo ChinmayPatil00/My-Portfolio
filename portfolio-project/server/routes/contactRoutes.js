52b3a8b7dbe405f26c4485762b584d835178d91f
ChinmayPatil <chinmaypatil1402@gmail.com>
2026-04-06 18:08:51 +0530

    removed submodule completely

--- portfolio-project/server/routes/contactRoutes.js
+++ portfolio-project/server/routes/contactRoutes.js
@@ -1,8 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
+const Contact = require("../models/Contact");
 
-const { sendMessage } = require('../controllers/contactController');
+router.post("/", async (req, res) => {
+  try {
+    console.log("API HIT ✅");
 
-router.post('/', sendMessage);   // ✅ THIS IS IMPORTANT
+    const { name, email, message } = req.body;
+
+    const newContact = new Contact({ name, email, message });
+     await newContact.save();
+
+    // Email setup
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: "New Contact Message",
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
+
+    res.status(200).json({
+      message: "Message sent successfully ✅",
+    });
+       
+    router.get("/", async (req, res) => {
+  const data = await Contact.find();
+  res.json(data);
+});
+    
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error sending message ❌",
+    });
+  }
+});
 
 module.exports = router;
--- portfolio-project/server/server.js
+++ portfolio-project/server/server.js
@@ -1,48 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+console.log("EMAIL:", process.env.EMAIL);
+console.log("PASS:", process.env.PASS);
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose =  require('mongoose');
-const projectRoutes = require('./routes/projectRoutes');
-const contactRoutes = require('./routes/contactRoutes');
-
-
+const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
-// THEN ROUTES
-app.use('/api/projects', projectRoutes);
-app.use('/api/contact', contactRoutes);
+// Routes
+app.use("/api/contact", contactRoutes);
 
-//Routes
-app.get('/', (req, res) =>  {
-    res.send("Backend is running 🚀");
-});
-
-app.get('/about', (req,res) => {
-    res.send("This is my portfolio backend");
-});
-
-app.get('/name', (req, res) => {
-    res.send("My name is Chinmay");
-});
-
-app.post('/api/contact', (req, res) => {
-  console.log("🔥 SERVER DIRECT HIT");
-  res.send("Server route working");
-});
-
-// start server
-const PORT = 5000;
-
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch(err => console.log(err));
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
 
-app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Backend working ✅");
+});
+
+// Server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
